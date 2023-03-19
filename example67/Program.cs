9c0b33658cbb2028cb94e5b1d4f61907e1aad1e5
JuliaRomaeva <julia.romaeva@gmail.com>
2023-03-19 21:15:14 +0300

    рекурсия сумма цифр

---
+++ example67/Program.cs
@@ -0,0 +1,32 @@
+﻿/***Задача 67:**Напишите программу,
+которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int Sum(int n){
+    int sum = 0;
+   if( n < 0){
+    n = n * -1;
+   }
+   while(n > 0){
+    sum = sum + n % 10;
+    n /= 10;
+   }
+   return sum;
+}
+int result = Sum(n);
+Console.Write($" {n} -> {result}");
+
+int SumNumbers(int num)
+{
+    if (num == 0)
+    return 0;
+    return(num % 10 + SumNumbers(num / 10));
+}
+Console.WriteLine();
+Console.WriteLine($" {n} -> {SumNumbers(n)}");
